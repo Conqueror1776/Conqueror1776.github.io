20e283d4a683bab78a012289b5963a4c1c59ee13
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-15 20:14:25 -0400

    Updates

---
+++ precache-manifest.dcf0452d9e7d42e1aadf67c236d4cfcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6bc32252162fde9ce98373d09ccd7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e26767b263ebcc51aef7",
+    "url": "/static/css/main.5366585d.chunk.css"
+  },
+  {
+    "revision": "a03b8bff38561ab61ee2",
+    "url": "/static/js/2.b6aeb93a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b6aeb93a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e26767b263ebcc51aef7",
+    "url": "/static/js/main.2048b51b.chunk.js"
+  },
+  {
+    "revision": "9d7ec453320509f6d66c",
+    "url": "/static/js/runtime-main.42cfb24a.js"
+  },
+  {
+    "revision": "e833f6928401d718b0056c559b752d59",
+    "url": "/static/media/img1.e833f692.png"
+  }
+]);
